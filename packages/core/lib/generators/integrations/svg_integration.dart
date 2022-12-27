460887af8d6dcca913570c502667bbc7abd6b338
Tomasz Marzeion <tomasz.marzeion@gostudent.org>
2022-12-27 15:03:08 +0100

    Nulldanger

--- packages/core/lib/generators/integrations/svg_integration.dart
+++ packages/core/lib/generators/integrations/svg_integration.dart
@@ -24,23 +24,23 @@ class SvgIntegration extends Integration {
   final String _assetName;
 
   SvgPicture svg({
-    Key? key,
+    Key key,
     bool matchTextDirection = false,
-    AssetBundle? bundle,
-    String? package$packageExpression,
-    double? width,
-    double? height,
+    AssetBundle bundle,
+    String package$packageExpression,
+    double width,
+    double height,
     BoxFit fit = BoxFit.contain,
     AlignmentGeometry alignment = Alignment.center,
     bool allowDrawingOutsideViewBox = false,
-    WidgetBuilder? placeholderBuilder,
-    Color? color,
+    WidgetBuilder placeholderBuilder,
+    Color color,
     BlendMode colorBlendMode = BlendMode.srcIn,
-    String? semanticsLabel,
+    String semanticsLabel,
     bool excludeFromSemantics = false,
     Clip clipBehavior = Clip.hardEdge,
     bool cacheColorFilter = false,
-    SvgTheme? theme,
+    SvgTheme theme,
   }) {
     return SvgPicture.asset(
       _assetName,
